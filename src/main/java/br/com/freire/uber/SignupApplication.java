8cb2a10677009c98e91feeb975652e7076db5463
Francisco Freire <franciscocfreire@gmail.com>
2024-06-09 09:24:33 -0300

    Add getAccount and tests

--- src/main/java/br/com/freire/uber/SignupApplication.java
+++ src/main/java/br/com/freire/uber/SignupApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 import java.util.UUID;
 import java.util.regex.Pattern;
 
@@ -21,7 +22,7 @@ public class SignupApplication {
     }
 
     @PostMapping("/signup")
-    public Response signup(@RequestBody SignupRequest request) {
+    public SignupResponse signup(@RequestBody SignupRequest request) {
         try {
             UUID id = UUID.randomUUID();
 
@@ -36,48 +37,66 @@ public class SignupApplication {
                                 if (Pattern.matches("[A-Z]{3}[0-9]{4}", request.getCarPlate())) {
                                     jdbcTemplate.update("INSERT INTO cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) VALUES (?, ?, ?, ?, ?, ?, ?)",
                                             id, request.getName(), request.getEmail(), request.getCpf(), request.getCarPlate(), request.isPassenger(), request.isDriver());
-                                    return new Response(id.toString());
+                                    return new SignupResponse(id.toString());
                                 } else {
-                                    return new Response(-5);
+                                    return new SignupResponse(-5);
                                 }
                             } else {
                                 jdbcTemplate.update("INSERT INTO cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) VALUES (?, ?, ?, ?, ?, ?, ?)",
                                         id, request.getName(), request.getEmail(), request.getCpf(), request.getCarPlate(), request.isPassenger(), request.isDriver());
-                                return new Response(id.toString());
+                                return new SignupResponse(id.toString());
                             }
                         } else {
-                            return new Response(-1);
+                            return new SignupResponse(-1);
                         }
                     } else {
-                        return new Response(-2);
+                        return new SignupResponse(-2);
                     }
                 } else {
-                    return new Response(-3);
+                    return new SignupResponse(-3);
                 }
             } else {
-                return new Response(-4);
+                return new SignupResponse(-4);
             }
         } finally {
             // No need to close connection explicitly, managed by Spring
         }
     }
 
+    @GetMapping("/accounts/{accountId}")
+    public AccountResponse getAccount(@PathVariable String accountId){
+        String sql = "SELECT * FROM cccat16.account WHERE account_id = ?";
+        Map<String, Object> result = jdbcTemplate.queryForMap(sql, accountId);
+
+        AccountResponse accountResponse = new AccountResponse();
+        accountResponse.setAccountId(((UUID) result.get("account_id")).toString());
+        accountResponse.setName((String) result.get("name"));
+        accountResponse.setEmail((String) result.get("email"));
+        accountResponse.setCpf((String) result.get("cpf"));
+        accountResponse.setCarPlate((String) result.get("car_plate"));
+        accountResponse.setPassenger((Boolean) result.get("is_passenger"));
+        accountResponse.setDriver((Boolean) result.get("is_driver"));
+
+        return accountResponse;
+
+    }
+
     private boolean validateCpf(String cpf) {
         return CpfValidator.validate(cpf);
     }
 
-    public static class Response {
+    public static class SignupResponse {
         private String accountId;
         private int errorCode;
 
         // Construtor padrão
-        public Response() {}
+        public SignupResponse() {}
 
-        public Response(String accountId) {
+        public SignupResponse(String accountId) {
             this.accountId = accountId;
         }
 
-        public Response(int errorCode) {
+        public SignupResponse(int errorCode) {
             this.errorCode = errorCode;
         }
 
@@ -87,6 +106,74 @@ public class SignupApplication {
 
         public int getErrorCode() {
             return errorCode;
+        }
+    }
+
+    public class AccountResponse {
+        private String accountId;
+        private String name;
+        private String email;
+        private String cpf;
+        private String carPlate;
+        private boolean isPassenger;
+        private boolean isDriver;
+
+        // Getters e Setters
+
+        public String getAccountId() {
+            return accountId;
+        }
+
+        public void setAccountId(String accountId) {
+            this.accountId = accountId;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getCpf() {
+            return cpf;
+        }
+
+        public void setCpf(String cpf) {
+            this.cpf = cpf;
+        }
+
+        public String getCarPlate() {
+            return carPlate;
+        }
+
+        public void setCarPlate(String carPlate) {
+            this.carPlate = carPlate;
+        }
+
+        public boolean isPassenger() {
+            return isPassenger;
+        }
+
+        public void setPassenger(boolean isPassenger) {
+            this.isPassenger = isPassenger;
+        }
+
+        public boolean isDriver() {
+            return isDriver;
+        }
+
+        public void setDriver(boolean isDriver) {
+            this.isDriver = isDriver;
         }
     }
 
--- src/test/java/br/com/freire/uber/SignupApplicationTests.java
+++ src/test/java/br/com/freire/uber/SignupApplicationTests.java
@@ -1,15 +1,14 @@
 package br.com.freire.uber;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -23,27 +22,42 @@ public class SignupApplicationTests {
     private final RestTemplate restTemplate = new RestTemplate();
 
     @Test
+    @DisplayName("Deve criar uma conta para o passageiro")
     void deveCriarContaParaPassageiro() {
         int expectedErrorCode = 0;
-        String url = "http://localhost:" + port + "/signup";
+        String expectedName = "John Doe";
+        String expectedEmail = "john.doe" + Math.random() + "@gmail.com";
+        String expectedCpf = "87748248800";
+
+        String urlSignup = "http://localhost:" + port + "/signup";
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json");
 
         SignupApplication.SignupRequest request = new SignupApplication.SignupRequest();
-        request.setName("John Doe");
-        request.setEmail("john.doe" + Math.random() + "@gmail.com");
-        request.setCpf("87748248800");
+        request.setName(expectedName);
+        request.setEmail(expectedEmail);
+        request.setCpf(expectedCpf);
         request.setPassenger(true);
         request.setDriver(false);
 
         HttpEntity<SignupApplication.SignupRequest> entity = new HttpEntity<>(request, headers);
 
-        ResponseEntity<SignupApplication.Response> response = restTemplate.exchange(url, HttpMethod.POST, entity, SignupApplication.Response.class);
+        ResponseEntity<SignupApplication.SignupResponse> responseSignup = restTemplate.exchange(urlSignup, HttpMethod.POST, entity, SignupApplication.SignupResponse.class);
 
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertNotNull(response.getBody());
-        assertNotNull(response.getBody().getAccountId());
+        assertEquals(HttpStatus.OK, responseSignup.getStatusCode());
+        assertNotNull(responseSignup.getBody());
+        assertNotNull(responseSignup.getBody().getAccountId());
 
-        assertEquals(expectedErrorCode, response.getBody().getErrorCode());
+        assertEquals(expectedErrorCode, responseSignup.getBody().getErrorCode());
+
+        String urlAccount = "http://localhost:" + port + "/accounts/" + responseSignup.getBody().getAccountId();
+        ResponseEntity<Map<String, Object>> responseGetAccount = restTemplate.exchange(urlAccount, HttpMethod.GET, null, new ParameterizedTypeReference<>() {
+        });
+        Map<String, Object> outputGetAccount = responseGetAccount.getBody();
+        assertEquals(expectedName, outputGetAccount.get("name"));
+        assertEquals(expectedEmail, outputGetAccount.get("email"));
+        assertEquals(expectedCpf, outputGetAccount.get("cpf"));
+
+
     }
 }
